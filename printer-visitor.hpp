fe7d1f397694390afa5cee05d7d81a724cc1f0a9
Ferhat Erata <ferhat.erata@yale.edu>
2019-10-25 01:46:48 -0400

    second version of the visitor done

---
+++ printer-visitor.hpp
@@ -0,0 +1,10 @@
+//  ----------------------------------------------------------------------------
+//  Header file for the PrinterVisitor class.        PrinterVisitor.hpp
+//  Created by Ferhat Erata <ferhat.erata@yale.edu> on October 24, 2019.
+//  Copyright (c) 2019 Yale University. All rights reserved.
+// -----------------------------------------------------------------------------
+
+#ifndef VC_GEN_PRINTERVISITOR_HPP
+#define VC_GEN_PRINTERVISITOR_HPP
+
+#endif // VC_GEN_PRINTERVISITOR_HPP
